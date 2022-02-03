2abe84c2fc71f8fb92703ee3de59e310d35d9632
jf4600 <98861587+jf4600@users.noreply.github.com>
2022-02-02 21:01:40 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -7,15 +7,6 @@ def welcome_assignment_answers(question):
     #Another way to implement is using a "case" statements similar to C.
     if question == "Are encoding and encryption the same? - Yes/No":
         answer = "No"
-    elif question == "Is it possible to decrypt a message without a key? - Yes/No":
-        answer = "No"
-    if question == "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted " \
-                   "by a TA?":
-        answer = "mtls"
-    elif question == "Is it possible to decode a message without a key? - Yes/No":
-        answer = "Yes"
-    if question == "Is a hashed message supposed to be un-hashed? - Yes/No":
-        answer = "No"
     elif question == "What is the MD5 hashing value to the following message: 'NYU Computer Networking' - Use MD5 " \
                      "hash generator and use the answer in your code":
         answer = "42b76fe51778764973077a5a94056724"
@@ -27,6 +18,15 @@ def welcome_assignment_answers(question):
     if question == "What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric " \
                    "number":
         answer = "4"
+    elif question == "Is it possible to decrypt a message without a key? - Yes/No":
+        answer = "No"
+    if question == "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted " \
+                   "by a TA?":
+        answer = "mtls"
+    elif question == "Is it possible to decode a message without a key? - Yes/No":
+        answer = "Yes"
+    if question == "Is a hashed message supposed to be un-hashed? - Yes/No":
+        answer = "No"
     return(answer)
 # Complete all the questions.
 
